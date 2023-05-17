f3747dec11542a4411350ccce32da65376fdc62a
wahidanik <wahid.anik@outlook.com>
2023-05-17 22:00:49 +0600

    footer added

--- src/Common/Footer/Footer.jsx
+++ src/Common/Footer/Footer.jsx
@@ -2,9 +2,33 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>This is footer</h1>
-        </div>
+        <footer className="footer p-10 bg-base-200 text-base-content">
+            <div>
+                
+                <h1 className='text-5xl font-semibold'>GoPlay</h1>
+                <p>GoPlay Industries Ltd.<br />Providing reliable toys since 2023</p>
+            </div>
+            <div>
+                <span className="footer-title">Services</span>
+                <a className="link link-hover">Branding</a>
+                <a className="link link-hover">Shop</a>
+                <a className="link link-hover">sales</a>
+                <a className="link link-hover">Advertisement</a>
+            </div>
+            <div>
+                <span className="footer-title">Company</span>
+                <a className="link link-hover">About us</a>
+                <a className="link link-hover">Contact</a>
+                <a className="link link-hover">Jobs</a>
+                <a className="link link-hover">Press kit</a>
+            </div>
+            <div>
+                <span className="footer-title">Legal</span>
+                <a className="link link-hover">Terms of use</a>
+                <a className="link link-hover">Privacy policy</a>
+                <a className="link link-hover">Cookie policy</a>
+            </div>
+        </footer>
     );
 };
 
